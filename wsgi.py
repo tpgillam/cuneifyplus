23eb250d47e4fc28dbcbb3e7f0b27b378efc6666
Thomas Gillam <tpgillam@googlemail.com>
2016-01-10 22:09:52 +0000

    Test

--- wsgi.py
+++ wsgi.py
@@ -12,11 +12,13 @@ def application(environ, start_response):
 {}
 </body></html>'''
 
-    cache_file_path = os.path.join(environ['OPENSHIFT_DATA_DIR'], 'cuneiform_cache.pickle')
-    body = str(os.listdir(environ['OPENSHIFT_DATA_DIR']))
-    body += '\n'
-    body += os.stat(cache_file_path)
     try:
+        cache_file_path = os.path.join(environ['OPENSHIFT_DATA_DIR'], 'cuneiform_cache.pickle')
+        body = str(os.listdir(environ['OPENSHIFT_DATA_DIR']))
+        body += '\n'
+        body += os.stat(cache_file_path)
+
+
         with FileCuneiformCache(cache_file_path=cache_file_path) as cache:
             cuneiform = cuneify_line(cache, 'd-un KESZ2', False)
         body = cuneiform
