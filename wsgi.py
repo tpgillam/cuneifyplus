20db3d29369da8b6178a654862701efb60dd1baf
Thomas Gillam <tpgillam@googlemail.com>
2016-01-10 16:34:55 +0000

    Add debuggin

--- wsgi.py
+++ wsgi.py
@@ -10,9 +10,12 @@ def application(environ, start_response):
 {}
 </body></html>'''
 
-    with FileCuneiformCache(cache_file_path='cuneiform_cache.pickle') as cache:
-        cuneiform = cuneify_line(cache, 'd-un KESZ2', False)
-    response_body = response_body.format(cuneiform)
+    try:
+        with FileCuneiformCache(cache_file_path='cuneiform_cache.pickle') as cache:
+            cuneiform = cuneify_line(cache, 'd-un KESZ2', False)
+        response_body = response_body.format(cuneiform)
+    except Exception as exc:
+        response_body = response_body.format(exc)
     response_body = response_body.encode('utf-8')
 
     status = '200 OK'
