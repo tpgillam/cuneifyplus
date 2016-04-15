087abc015d012084a2f25d2d49da63b6825be981
Thomas Gillam <tpgillam@googlemail.com>
2016-04-15 23:16:34 +0100

    Tidy up

--- wsgi.py
+++ wsgi.py
@@ -53,36 +53,19 @@ def _get_cuneify_body(environ, transliteration, show_transliteration):
 
 def application(environ, start_response):
     ''' Entry point for the application '''
-
     # Use the appropriate behaviour here
     path_info = environ['PATH_INFO']
-    # parameters = parse_qs(environ['QUERY_STRING'])
     form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ, keep_blank_values=True)
-    print(form)
-    for thing in form:
-        print('thing')
-
-    print('path_info:', path_info)
     if path_info == '/cuneify':
-        # FIXME getvalue returns None if it can't find anything, so remove
-        # try-except and replace with an if statement
-        try:
-            # Not sure why the form requires us to take the zeroth element
-            # transliteration = parameters['input'][0]
-            transliteration = form.getvalue('input')
-            print(transliteration)
-        except KeyError:
-            # print(form)
-            # print(form['input'])
-            # raise
+        transliteration = form.getvalue('input')
+        if transliteration is None:
+            # There is no transliteration, so show the input form again
             body = _get_input_form()
         else:
-            # show_transliteration = 'show_transliteration' in parameters
-            show_transliteration = form.getvalue('show_transliteration')
+            show_transliteration = form.getvalue('show_transliteration').lower() == 'on'
             body = _get_cuneify_body(environ, transliteration, show_transliteration)
     else:
         body =  _get_input_form()
-
 
     response_body = '''<!doctype html>
 <html lang="en">
