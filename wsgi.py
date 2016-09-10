dd0c369cd33689c74eb8b072f9dd8ebbc08b0f31
Thomas Gillam <tpgillam@googlemail.com>
2016-09-10 18:32:56 +0100

    Fix bug

--- wsgi.py
+++ wsgi.py
@@ -42,7 +42,7 @@ def _get_input_form(initial='Enter transliteration here...'):
     return body
 
 
-def _cache_file_path():
+def _cache_file_path(environ):
     ''' Return the standard cuneiform cache file path '''
     # We use a cache in the data directory. This isn't touched by the deployment process
     return os.path.normpath(os.path.join(environ['OPENSHIFT_DATA_DIR'], 'cuneiform_cache.pickle'))
@@ -52,7 +52,7 @@ def _get_cuneify_body(environ, transliteration, show_transliteration, font_name)
     ''' Return the HTML body contents when we've been given a transliteration, and show in the specified font '''
     body = ''
     try:
-        with FileCuneiformCache(cache_file_path=_cache_file_path()) as cache:
+        with FileCuneiformCache(cache_file_path=_cache_file_path(environ)) as cache:
             for line in transliteration.split('\n'):
                 # Make empty lines appear as breaks in the output
                 line = line.strip()
@@ -83,7 +83,7 @@ def _get_cuneify_body(environ, transliteration, show_transliteration, font_name)
 def _get_symbol_list_body(environ, transliteration, font_name):
     ''' Return the HTML body for the symbol list page '''
     body = ''
-    with FileCuneiformCache(cache_file_path=_cache_file_path()) as cache:
+    with FileCuneiformCache(cache_file_path=_cache_file_path(environ)) as cache:
         try:
             for cuneiform_symbol, transliterations in ordered_symbol_to_transliterations(cache, transliteration).iteritems():
                 line = '<span class="{}">{}</span><br />'.format(font_name.lower(), cuneiform_symbol, ', '.join(transliterations))
